523d5d9aa671ef2b65cca56ac37d8cf36f415638
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-02 02:35:42 +0100

    add app colors

---
+++ lib/core/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff00bf63);
+  static const Color secondaryColor = Color(0xffFFF089);
+  static const Color starkWhite = Color(0xffffffff);
+  static const Color leadBlack = Color(0xff000000);
+}
