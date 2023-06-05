6c1befe79a0869934608edfb04f16c6614fdb616
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-05 23:59:55 +0100

    add dialog for signup

--- lib/features/auth/presentation/screens/get_started_screen.dart
+++ lib/features/auth/presentation/screens/get_started_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
+import 'package:task_buddy/features/auth/presentation/widgets/sign_up_dialog.dart';
 
 import '../../../../core/colors/app_colors.dart';
 import '../../../../widgets/app_text.dart';
+import '../../../../widgets/button.dart';
 
 class GetStartedScreen extends StatefulWidget {
   const GetStartedScreen({Key? key}) : super(key: key);
@@ -12,82 +14,82 @@ class GetStartedScreen extends StatefulWidget {
 }
 
 class _GetStartedScreenState extends State<GetStartedScreen> {
+  bool isDialogShown = false;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Column(
-              children: [
-                const SizedBox(height: 50,),
-                const AppText(text: 'Get Started!',
-                  color: AppColors.starkWhite,size: 28, fontWeight: FontWeight.w700,),
-                const SizedBox(height: 30,),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  height: size.height/2.5,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.starkWhite,
-                  ),
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: Column(
+                        children: [
+                          const SizedBox(height: 50,),
+                          const AppText(text: 'Get Started!',
+                            color: AppColors.starkWhite,size: 28, fontWeight: FontWeight.w700,),
+                          const SizedBox(height: 30,),
+                          Container(
+                            alignment: Alignment.bottomCenter,
+                            height: isDialogShown? size.height/2.5: size.height/3.5,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppColors.starkWhite,
+                            ),
 
-                  child: const RiveAnimation.asset(
-                    "assets/rive_assets/onboarding.riv",
-                    artboard: "buddy",
-                  ),
-                ),
-                const Spacer(),
-                TextButton(
-                  onPressed: () {
+                            child: const RiveAnimation.asset(
+                              "assets/rive_assets/onboarding.riv",
+                              artboard: "buddy",
+                            ),
+                          ),
+                          const Spacer(),
+                          Button(
+                            text: 'Sign Up',
+                            press: () {
+                                  setState(() {
+                                    isDialogShown = true;
+                                  });
 
-                  },
-                  style: TextButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))
-                      )
-                  ),
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50,
-                    width: 250,
-                    decoration: const BoxDecoration(
-                        color: AppColors.secondaryColor,
-                        borderRadius: BorderRadius.all(Radius.circular(30))
-                    ),
-                    child: const AppText(text: 'Sign Up',
-                      color: AppColors.leadBlack,size: 18, fontWeight: FontWeight.w400,),
-                  ),
-                ),
+                            },
+                          ),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const AppText(text: 'Already have an account?',
-                      color: AppColors.starkWhite,size: 16, fontWeight: FontWeight.w700,),
-                    TextButton(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const AppText(text: 'Already have an account?',
+                                color: AppColors.starkWhite,size: 16, fontWeight: FontWeight.w700,),
+                              TextButton(
 
-                      onPressed: () {
+                                onPressed: () {
 
-                      },
-                      style: TextButton.styleFrom(
-                          shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(30))
-                          )
+                                },
+                                style: TextButton.styleFrom(
+                                    shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(Radius.circular(30))
+                                    )
+                                ),
+                                child: const AppText(text: 'Log in',
+                                  color: AppColors.secondaryColor,size: 16, fontWeight: FontWeight.w700,),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      child: const AppText(text: 'Log in',
-                        color: AppColors.secondaryColor,size: 16, fontWeight: FontWeight.w700,),
-                    ),
-                  ],
-                ),
-              ],
+
+              ),
             ),
-          ),
+            AnimatedPositioned(
+
+              child: CustomSignupDialog(),
+              duration: const Duration(milliseconds: 400),),
+          ],
         ),
       ),
     );
   }
 }
+
+
