ac9004c828f732d93238decb11205daecff3803c
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-06 19:27:09 +0100

    - add toggle between login and sign up dialogs

--- lib/features/auth/presentation/screens/get_started_screen.dart
+++ lib/features/auth/presentation/screens/get_started_screen.dart
@@ -5,6 +5,7 @@ import 'package:task_buddy/features/auth/presentation/widgets/sign_up_dialog.dar
 import '../../../../core/colors/app_colors.dart';
 import '../../../../widgets/app_text.dart';
 import '../../../../widgets/button.dart';
+import '../widgets/log_in_dialog.dart';
 
 class GetStartedScreen extends StatefulWidget {
   const GetStartedScreen({Key? key}) : super(key: key);
@@ -14,11 +15,13 @@ class GetStartedScreen extends StatefulWidget {
 }
 
 class _GetStartedScreenState extends State<GetStartedScreen> {
-  bool isDialogShown = false;
+  bool isSignupDialogShown = false;
+  bool isLoginDialogShown = false;
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.primaryColor,
       body: SafeArea(
         child: Stack(
@@ -29,12 +32,14 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                 child: Column(
                         children: [
                           const SizedBox(height: 50,),
-                          const AppText(text: 'Get Started!',
+                          isSignupDialogShown? Container(): isLoginDialogShown?Container()
+                          :const AppText(text: 'Get Started!',
                             color: AppColors.starkWhite,size: 28, fontWeight: FontWeight.w700,),
                           const SizedBox(height: 30,),
                           Container(
                             alignment: Alignment.bottomCenter,
-                            height: isDialogShown? size.height/2.5: size.height/3.5,
+                            height: isSignupDialogShown? size.height/6:
+                                    isLoginDialogShown? size.height/4.5: size.height/2.5,
                             decoration: const BoxDecoration(
                               shape: BoxShape.circle,
                               color: AppColors.starkWhite,
@@ -50,7 +55,7 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                             text: 'Sign Up',
                             press: () {
                                   setState(() {
-                                    isDialogShown = true;
+                                    isSignupDialogShown = true;
                                   });
 
                             },
@@ -64,7 +69,9 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
                               TextButton(
 
                                 onPressed: () {
-
+                                  setState(() {
+                                    isLoginDialogShown = true;
+                                  });
                                 },
                                 style: TextButton.styleFrom(
                                     shape: const RoundedRectangleBorder(
@@ -82,9 +89,77 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
               ),
             ),
             AnimatedPositioned(
+              bottom: isSignupDialogShown? -size.height/20:-size.height/1.4,
+              duration: const Duration(milliseconds: 400),
+              child: CustomSignupDialog(pressed: () {
+                setState(() {
+                  isSignupDialogShown = false;
+                  isLoginDialogShown = true;
 
-              child: CustomSignupDialog(),
-              duration: const Duration(milliseconds: 400),),
+                });
+
+              },)),
+            AnimatedPositioned(
+                bottom: isLoginDialogShown? -size.height/20:-size.height/1.4,
+                duration: const Duration(milliseconds: 400),
+                child: CustomLoginDialog(pressed: () {
+                  setState(() {
+                    isSignupDialogShown = true;
+                    isLoginDialogShown = false;
+
+                  });
+                },)),
+
+            isSignupDialogShown?
+                  TextButton(
+              style: TextButton.styleFrom(
+                shape: const CircleBorder()
+                ),
+              onPressed: () {
+                setState(() {
+                  isSignupDialogShown = false;
+                  isLoginDialogShown = false;
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.starkWhite,
+                ),
+                child: const Icon(Icons.arrow_back_sharp,
+                  size: 30,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            )
+                  :Container(),
+            isLoginDialogShown?
+                TextButton(
+              style: TextButton.styleFrom(
+                  shape: const CircleBorder()
+              ),
+              onPressed: () {
+                setState(() {
+                  isLoginDialogShown = false;
+                });
+              },
+              child: Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: AppColors.starkWhite,
+                ),
+                child: const Icon(Icons.arrow_back_sharp,
+                  size: 30,
+                  color: AppColors.primaryColor,
+                ),
+              ),
+            )
+                :Container()
+
           ],
         ),
       ),
