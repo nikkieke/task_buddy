85144b29bbedeaf39f5d8e08f32ae38d658f1f18
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-06 19:25:15 +0100

    add color to app colors

--- lib/core/colors/app_colors.dart
+++ lib/core/colors/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color secondaryColor = Color(0xffFFF089);
   static const Color starkWhite = Color(0xffffffff);
   static const Color leadBlack = Color(0xff000000);
+  static const Color lightGrey = Color(0xffcfc9c8);
 }
