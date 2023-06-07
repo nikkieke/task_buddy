44532a71874177702da33cd1c26bdd122f7103f3
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-07 21:59:18 +0100

    add auth repository implementation class

---
+++ lib/features/auth/infrastructure/repository/auth_repository_impl.dart
@@ -0,0 +1,24 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart';
+import '../../../../core/service/appwrite_client.dart';
+import '../../domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository{
+  @override
+  Future<User> createUser(String userId, String name, String email, String password)async {
+    Account account = Account(AppWriteClient.instance.client);
+
+    try{
+      final response = await account
+          .create(userId: userId,
+          name: name,
+          email: email,
+          password: password);
+      return response;
+
+    }on AppwriteException catch(e){
+      throw e.response.toString();
+    }
+  }
+
+}
