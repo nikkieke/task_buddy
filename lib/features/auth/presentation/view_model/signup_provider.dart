fe299246add479959981055a9626a7cb0170f424
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-07 22:04:03 +0100

    add view model signup provider

---
+++ lib/features/auth/presentation/view_model/signup_provider.dart
@@ -0,0 +1,61 @@
+
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:task_buddy/widgets/app_text.dart';
+
+import '../../../../core/routes/routes.dart';
+import '../../domain/usecase/provider.dart';
+
+class SignupProvider extends ChangeNotifier{
+  final ChangeNotifierProviderRef ref;
+  
+  SignupProvider({required this.ref});
+  
+  bool loading = false;
+  final nameCtr = TextEditingController();
+  final emailCtr = TextEditingController();
+  final passwordCtr = TextEditingController();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  
+  Future<void>signup(BuildContext context)async{
+    loading = true;
+    notifyListeners();
+    final response = await ref.watch(signupProvider)
+        .signUp(
+        ID.unique(),
+        nameCtr.text,
+        emailCtr.text,
+        passwordCtr.text,
+    ).catchError((e){
+      if(context.mounted){
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: AppText(text: 'SignUp Failed! Please input valid details',)));
+      }
+      loading = false;
+      notifyListeners();
+      throw e;
+    });
+
+    print(response.status);
+    if(response.status == true){
+      if(context.mounted){
+        context.go(Routes.VERIFYEMAIL);
+      }
+
+    }
+
+      loading = false;
+      notifyListeners();
+    }
+
+
+
+  }
+  
+
+
+final signUpProvider = ChangeNotifierProvider<SignupProvider>((ref) => SignupProvider(ref: ref));
