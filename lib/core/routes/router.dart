c3a1715faa444e6cff10db05c8ba0a4f04839fa9
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-07 22:02:51 +0100

    add new routes and paths

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -5,6 +5,8 @@ import 'package:task_buddy/core/routes/routes.dart';
 import '../../features/auth/presentation/screens/get_started_screen.dart';
 import '../../features/auth/presentation/screens/onboarding.dart';
 import '../../features/auth/presentation/screens/splashscreen.dart';
+import '../../features/auth/presentation/screens/verify_email_screen.dart';
+import '../../features/home/presentation/screens/home.dart';
 
 final _rootNavigatorKey = GlobalKey<NavigatorState>();
 final _shellNavigatorKey = GlobalKey<NavigatorState>();
@@ -25,6 +27,16 @@ final router = GoRouter(
     GoRoute(
       path: Routes.GETSTARTED,
       builder: (context, state) => const GetStartedScreen(),
+    ),
+
+    GoRoute(
+      path: Routes.HOME,
+      builder: (context, state) => const HomeScreen(),
+    ),
+
+    GoRoute(
+      path: Routes.VERIFYEMAIL,
+      builder: (context, state) => const VerifyEmailScreen(),
     ),
 ]
 );
--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -3,6 +3,8 @@ abstract class Routes {
   static const SPLASHSCREEN = Paths.SPLASHSCREEN;
   static const ONBOARDING = Paths.ONBOARDING;
   static const GETSTARTED = Paths.GETSTARTED;
+  static const HOME = Paths.HOME;
+  static const VERIFYEMAIL = Paths.VERIFYEMAIL;
 
 
 
@@ -17,5 +19,7 @@ abstract class Paths {
   static const SPLASHSCREEN = '/splashscreen';
   static const ONBOARDING = '/onboarding';
   static const GETSTARTED = '/get-started';
+  static const HOME = '/home';
+  static const VERIFYEMAIL = '/verify-email';
 
 }
