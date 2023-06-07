b6c634019ac20f46ab16b6e84ca66272b8429fe5
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-07 21:59:43 +0100

    add auth usecase and usecase implementation class

---
+++ lib/features/auth/domain/usecase/auth_usecase.dart
@@ -0,0 +1,25 @@
+import '../repository/auth_repository.dart';
+import 'package:appwrite/models.dart';
+
+
+abstract class AuthUseCase{
+  Future<User>signUp(String userID, String name, String email, String password);
+  
+}
+
+
+
+
+
+
+class AuthUseCaseImpl implements AuthUseCase{
+  final AuthRepository authRepository;
+
+  AuthUseCaseImpl(this.authRepository);
+
+  @override
+  Future<User> signUp(String userID, String name, String email, String password) async{
+    return await authRepository.createUser(userID, name, email, password);
+  }
+  
+}
