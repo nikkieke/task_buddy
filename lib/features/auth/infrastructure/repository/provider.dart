3d91eae68ce7920b849f1dc609e7da753834c1c3
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-07 22:01:45 +0100

    add signup auth repository implementation provider

---
+++ lib/features/auth/infrastructure/repository/provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../domain/repository/auth_repository.dart';
+import 'auth_repository_impl.dart';
+
+final createAcctProvider = Provider<AuthRepository>((ref){
+  return AuthRepositoryImpl();
+});
