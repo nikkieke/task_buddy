11665685004df4ccda9e89a087fe1f9610209780
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-09 23:55:32 +0100

    add signin function to abstract class

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,4 +2,6 @@ import 'package:appwrite/models.dart';
 
 abstract class AuthRepository{
   Future<User>createUser(String userID, String name, String email, String password);
+  Future<Session>signInUser(String email, String password);
+
 }
