e46d5564afbabd3b5463b2ae746ca98750c169fd
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-10 00:00:02 +0100

    add project details page

---
+++ lib/features/home/presentation/screens/project_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProjectDetailsScreen extends StatefulWidget {
+  const ProjectDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProjectDetailsScreen> createState() => _ProjectDetailsScreenState();
+}
+
+class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
