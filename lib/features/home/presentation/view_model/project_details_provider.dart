263723322eb5cd314d0f729849098f33626749b0
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-14 01:55:53 +0100

    add project details provider for viewmodel

--- lib/features/home/presentation/view_model/project_details_provider.dart
+++ lib/features/home/presentation/view_model/project_details_provider.dart
@@ -1,19 +1,67 @@
+import 'package:appwrite/models.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../widgets/dialog_box_widget.dart';
+import '../../domain/usecase/task_provider.dart';
 
 class ProjectDetailsProvider extends ChangeNotifier{
   final ChangeNotifierProviderRef ref;
 
   ProjectDetailsProvider({required this.ref});
-
-  bool taskCompleted = false;
+  bool loading = false;
   final newTaskCtr = TextEditingController();
+  List <Document> list = [];
+  bool haveData = false;
 
-  completedToggle(bool? value) {
-    taskCompleted = value!;
+
+
+  Future<void>createTask(String projectID, context)async{
+    final response = await ref.watch(createNewTaskProvider).
+    createATask(
+        projectID,
+        newTaskCtr.text,
+    ).catchError((e){
+      notifyListeners();
+      throw e;
+    });
+    print(response);
+    getTaskList(projectID);
+    notifyListeners();
+  }
+
+  Future<void> getTaskList(String projectID)async{
+    list = await ref.watch(getAllTasksProvider)
+        .getAllTasks(projectID)
+        .catchError((e){
+      throw e;
+    });
+
+    print(list.toString());
+    haveData = true;
+    notifyListeners();
+  }
+
+  Future<void> deleteTaskItem(String documentId, index)async{
+    await ref.watch(deleteATaskProvider)
+        .deleteATask(documentId)
+        .catchError((e) {
+      throw e;
+    });
+    list.removeAt(index);
+    notifyListeners();
+  }
+
+  Future<void>updateDocument(String projectID, String documentId, bool isCompleted, )async{
+    final response = await ref.watch(editATaskProvider)
+        .updateATask
+      (
+        isCompleted,
+        documentId
+    ).catchError((e) {
+      throw e;
+    });
+    print(response);
+    getTaskList(projectID);
     notifyListeners();
   }
 
