1ffe9aa1411ead491e3f459d431203dc596b2deb
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-06-14 01:54:28 +0100

    change file name to config

---
+++ lib/core/service/config.dart
@@ -0,0 +1,8 @@
+
+class AppWriteConfig{
+  static const projectID = '64808bdd20440875c7d4';
+  static const endpoint = 'https://cloud.appwrite.io/v1';
+  static const databaseID = '648456262be5bca7312d';
+  static const projectsCollectionID = '6487733476503d802369';
+  static const tasksCollectionID = '6487b7ef4ae368e0f329';
+}
